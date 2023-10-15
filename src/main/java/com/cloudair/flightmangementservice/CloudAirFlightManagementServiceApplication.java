8f71a4cd93b71ae1efe8c57a41e6d44806d156f3
Subin Mathew <subinbm1995@gmail.com>
2023-10-15 23:22:37 +0530

    Initial Commit

---
+++ src/main/java/com/cloudair/flightmangementservice/CloudAirFlightManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cloudair.flightmangementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudAirFlightManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudAirFlightManagementServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cloudair/flightmangementservice/CloudAirFlightManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudair.flightmangementservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudAirFlightManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
